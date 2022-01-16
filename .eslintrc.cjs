0ea5877b0ff51b358e01116c70a3849dfcf47ec3
yiwwhl <yiwwhl@qq.com>
2022-01-16 13:06:53 +0800

    chore: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
   extends: [
     'eslint:recommended',  
     "plugin:@typescript-eslint/recommended",
-    "eslint-config-prettier",
     "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
