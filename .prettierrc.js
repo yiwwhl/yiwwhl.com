57370c485eeefac866ee31dbc47ceba27e897d98
yiwwhl <yiwwhl@qq.com>
2023-01-06 15:05:52 +0800

    chore: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80,
+  singleQuote: false,
+  quoteProps: "consistent",
+  htmlWhitespaceSensitivity: "strict",
+  vueIndentScriptAndStyle: true,
+  trailingComma: "all",
+};
