b0a714394d539ffcf548d21272ac2577596359ae
yiwwhl <yiwwhl@qq.com>
2023-01-17 13:18:17 +0800

    chore: update eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,15 @@
-const path = require("path");
-
 module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',  
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-  },
   env: {
-    "browser": true,
-    "node": true,
+    browser: true,
+    es2021: true,
   },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/triple-slash-reference": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/no-floating-promises": "off",
-    "comma-dangle": ["error", "always-multiline"],
-    "@typescript-eslint/restrict-template-expressions": "off",
-    "@typescript-eslint/promise-function-async": "off",
-    "prefer-regex-literals": "off",
-    "@typescript-eslint/consistent-type-assertions": "off",
-    "@typescript-eslint/restrict-plus-operands": "error",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
+  extends: ["plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
   },
-  ignorePatterns: ["dist/**/*", "src/assets/icons/iconfont.js", ".eslintrc.js", "node_modules/**", "config/**", "src/utils/is.ts"],
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
 };
