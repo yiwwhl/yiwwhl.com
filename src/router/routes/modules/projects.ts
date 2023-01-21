98e2725657827d6af23f6d224880abdf97e8ea43
yiwwhl <yiwwhl@qq.com>
2023-01-21 13:30:07 +0800

    feat: add router module projects

---
+++ src/router/routes/modules/projects.ts
@@ -0,0 +1,19 @@
+import { DEFAULT_LAYOUT } from "@/router/base";
+
+const Projects = {
+  path: "/projects",
+  name: "Projects",
+  component: DEFAULT_LAYOUT,
+  redirect: {
+    name: "ProjectsList",
+  },
+  children: [
+    {
+      path: "list",
+      name: "ProjectsList",
+      component: () => import("@/views/Projects/List/index"),
+    },
+  ],
+};
+
+export default Projects;
