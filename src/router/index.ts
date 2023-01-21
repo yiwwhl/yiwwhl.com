83810abf50f063ed9a5caaf1184ba7953962ac49
yiwwhl <yiwwhl@qq.com>
2023-01-21 13:29:17 +0800

    feat: update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { DEFAULT_LAYOUT } from "./base";
+import { moduleRoutes } from "./routes";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
-      component: () => import("../views/HomePage/index"),
+      component: DEFAULT_LAYOUT,
+      meta: {
+        locale: "首页",
+        order: 0,
+      },
+      redirect: {
+        name: "HomePage",
+      },
+      children: [
+        {
+          path: "homepage",
+          name: "HomePage",
+          component: () => import("@/views/HomePage/index"),
+          meta: {
+            locale: "首页",
+            requiresAuth: true,
+          },
+        },
+      ],
     },
+    ...moduleRoutes,
   ],
 });
 
