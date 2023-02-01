545bf0e225ea04f68b7135684746eb6749655967
yiwwhl <yiwwhl@qq.com>
2023-02-01 22:13:32 +0800

    chore: import arco design

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from "./App.jsx";
 import { useTitleWithDefault } from "./hooks/interface";
 import router from "./router";
 import pinia from "./store";
+import ArcoVue from "@arco-design/web-vue";
 
 import "@/assets/style/base.scss";
+import "@arco-design/web-vue/dist/arco.css";
 
 /**
  * init
@@ -15,5 +17,6 @@ const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.use(ArcoVue);
 
 app.mount("#app");
--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -3,8 +3,19 @@ import { useInterfaceStore } from "../../store";
 export default defineComponent({
   setup() {
     const interfaceStore = useInterfaceStore();
+    const content = () => {
+      return (
+        <a-button
+          style={{ marginLeft: "20px", marginTop: "20px" }}
+          type="primary"
+          onClick={() => alert("hey")}
+        >
+          Primary
+        </a-button>
+      );
+    };
     return () => {
-      return <h1>{JSON.stringify(interfaceStore)}</h1>;
+      return content();
     };
   },
 });
