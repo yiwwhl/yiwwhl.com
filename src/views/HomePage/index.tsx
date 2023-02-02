4e30216cd6e81c2d59ca8daca30cbe350900a5ab
yiwwhl <yiwwhl@qq.com>
2023-02-02 23:23:26 +0800

    feat: simple homepage

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -1,17 +1,16 @@
 import { useInterfaceStore } from "../../store";
+import styles from "./index.module.scss";
+import cyberpunkCoder from "@/assets/images/cyberpunkCoder.jpg";
 
 export default defineComponent({
   setup() {
     const interfaceStore = useInterfaceStore();
     const content = () => {
       return (
-        <a-button
-          style={{ marginLeft: "20px", marginTop: "20px" }}
-          type="primary"
-          onClick={() => alert("hey")}
-        >
-          Primary
-        </a-button>
+        <div>
+          <img src={cyberpunkCoder} class={styles.headerImage}></img>
+          <h1 style={{ marginLeft: "400px" }}>Welcome</h1>
+        </div>
       );
     };
     return () => {
