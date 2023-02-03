d3752cee87116bd39d540b20e2cc6569dc2d46d6
yiwwhl <yiwwhl@qq.com>
2023-02-03 21:48:30 +0800

    feat: add some headerImg

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -2,10 +2,14 @@ import { defineComponent } from "vue";
 import styles from "./index.module.scss";
 import avatar from "@/assets/images/avatar.png";
 import router from "@/router";
+import cyberpunkCoder from "@/assets/images/cyberpunkCoder.jpg";
+import city from "@/assets/images/city.png";
+import grass from "@/assets/images/grass.png";
 
 interface NavBarItem {
   routerName: string;
   label: string;
+  headerImg: string;
 }
 
 export default defineComponent({
@@ -14,40 +18,54 @@ export default defineComponent({
       {
         routerName: "HomePage",
         label: "Home",
+        headerImg: cyberpunkCoder,
       },
       {
         routerName: "ProjectsList",
         label: "Project",
+        headerImg: city,
       },
       {
         routerName: "About",
         label: "About",
+        headerImg: grass,
       },
     ];
 
-    const handleNavBarClick = (navBar: NavBarItem) => {
+    const currentNavBarIndex = ref(0);
+
+    const handleNavBarClick = (navBar: NavBarItem, index: number) => {
       try {
+        currentNavBarIndex.value = index;
         router.push({ name: navBar.routerName });
       } catch {
+        currentNavBarIndex.value = 0;
         router.push({ name: "NotFound" });
       }
     };
 
     return () => {
       return (
-        <div class={styles.contentWrapper}>
-          <div class={styles.navBarWrapper}>
-            {navBarList.map((navBar) => (
-              <div
-                class={styles.navBar}
-                onClick={() => handleNavBarClick(navBar)}
-              >
-                {navBar.label}
-              </div>
-            ))}
+        <>
+          <div class={styles.contentWrapper}>
+            <div class={styles.navBarWrapper}>
+              {navBarList.map((navBar, index) => (
+                <div
+                  class={styles.navBar}
+                  onClick={() => handleNavBarClick(navBar, index)}
+                >
+                  {navBar.label}
+                </div>
+              ))}
+            </div>
+            <img src={avatar} class={styles.avatar}></img>
           </div>
-          <img src={avatar} class={styles.avatar}></img>
-        </div>
+
+          <img
+            src={navBarList[currentNavBarIndex.value].headerImg}
+            class={styles.headerImg}
+          ></img>
+        </>
       );
     };
   },
--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -1,6 +1,5 @@
 import { useInterfaceStore } from "../../store";
 import styles from "./index.module.scss";
-import cyberpunkCoder from "@/assets/images/cyberpunkCoder.jpg";
 
 export default defineComponent({
   setup() {
@@ -8,7 +7,6 @@ export default defineComponent({
     const content = () => {
       return (
         <div>
-          <img src={cyberpunkCoder} class={styles.headerImage}></img>
           <h1 style={{ marginLeft: "400px" }}>Welcome</h1>
         </div>
       );
