d59645975beafb9e9168258518c22796e7d2554e
yiwwhl <yiwwhl@qq.com>
2023-02-03 14:42:26 +0800

    fix: vue router use hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { DEFAULT_LAYOUT } from "./base";
 import { moduleRoutes } from "./routes";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
