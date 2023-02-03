19c245691f34db7607796731d0eb6258e545f504
yiwwhl <yiwwhl@users.noreply.github.com>
2023-02-03 06:43:19 +0000

    deploy: d59645975beafb9e9168258518c22796e7d2554e

--- assets/index-275def5c.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as t,a as r}from"./index-77933312.js";const n=e({setup(){return()=>t("h1",null,[r("404...")])}});export{n as default};
--- assets/index-c7e9971d.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as t,a as r}from"./index-77933312.js";const a=e({setup(){return()=>t("h1",null,[r("Projects List")])}});export{a as default};
