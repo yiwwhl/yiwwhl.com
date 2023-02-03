55ad2bca53c0ec0337e09cb7fa10106fab7d4ddb
yiwwhl <yiwwhl@users.noreply.github.com>
2023-02-03 13:57:56 +0000

    deploy: a0245a0a3d0964e02aaa970d8b83766fa1b1c40c

--- assets/index-0f523e05.js
+++
@@ -1,1 +0,0 @@
-import{d as n,u as r,c as e,a as o}from"./index-d311bb44.js";const s=n({setup(){r();const t=()=>e("div",null,[e("h1",{style:{marginLeft:"400px"}},[o("Welcome")])]);return()=>t()}});export{s as default};
